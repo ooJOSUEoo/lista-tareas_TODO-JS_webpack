b4ed5db242234c5be6241d9b40eb280d691d9528
Josue <sjosue5082002@gmail.com>
2022-02-03 20:35:52 -0600

    Todo Class

---
+++ src/classes/todo.class.js
@@ -0,0 +1,11 @@
+export class Todo{
+
+    constructor(tarea){
+        this.tarea = tarea;
+
+        this.id = new Date().getTime();
+        this.completado = false;
+        this.creado = new Date();
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import { saludar } from './js/componentes';
 import './styles.css';
 
-const nombre = 'Fernando';
+import {Todo} from './classes/todo.class.js';
 
-saludar( nombre );
+
+const tarea = new Todo('Aprender JavaScript');
+
+
+console.log(tarea);
