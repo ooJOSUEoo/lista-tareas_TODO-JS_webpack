0d01d61bf4bcf96484c849e8031cb2811219ca6c
Josue <sjosue5082002@gmail.com>
2022-02-03 20:44:55 -0600

    TodoList Class

---
+++ src/classes/todo-list.class.js
@@ -0,0 +1,22 @@
+
+
+export class TodoList{
+
+    constructor(){
+        this.todos = [];
+    }
+
+    nuevoTodo(todo){
+        this.todos.push(todo);
+    }
+
+    eliminarTodo(id){
+    }
+
+    marcarCompletado(id){
+    }
+
+    eliminarCompletados(){
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+
 import './styles.css';
 
-import {Todo} from './classes/todo.class.js';
+import {Todo,TodoList} from './classes';
 
 
+const todoList = new TodoList();
 const tarea = new Todo('Aprender JavaScript');
+const tarea2 = new Todo('Aprender React');
 
+todoList.nuevoTodo(tarea);
+todoList.nuevoTodo(tarea2);
 
-console.log(tarea);
+console.log(todoList);
